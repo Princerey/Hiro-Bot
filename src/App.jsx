6200afbd42cb4abbd5f851baabd25e80f82a0a33
Princerey <rohkumar0126@gmail.com>
2024-01-04 03:06:12 +0530

    Search console verification

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,6 @@ import { Link } from 'react-router-dom';
 const App = () => {
   return (
     <>
-    <head>
-    <meta name="google-site-verification" content="M5AXSI6T05aBtOL0zN-RlAFcM4OHuGegv_NbjwxKTDg" />
-    </head>
       <main>
         <div className="main">
           <div className="gradient" />
